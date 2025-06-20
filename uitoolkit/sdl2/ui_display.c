c43835841cc9b2218beb0017dbd72b3138c06236
Chuhai <chu-hai@users.noreply.github.com>
2025-06-21 00:36:17 +0900

    SDL_SetWindowSizeはPTY切替時のラグになるので削除

--- uitoolkit/sdl2/ui_display.c
+++ uitoolkit/sdl2/ui_display.c
@@ -1380,7 +1380,9 @@ int ui_display_resize(ui_display_t *disp, u_int width, u_int height) {
     disp->display->resizing = 1;
 #ifdef USE_SDL2_KMSDRM
     if (!disp->display->parent) {
+#if 0
       SDL_SetWindowSize(disp->display->window, disp->display->width, disp->display->height);
+#endif
     } else {
       /* copy from SDL_WINDOWEVENT */
       if (width != disp->display->width || height != disp->display->height) {
